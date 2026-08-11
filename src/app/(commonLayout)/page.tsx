e746636a9ae2325021a810e1e1e43033f690608f
ijazhossain <ijaz.hossain35@gmail.com>
2026-08-11 12:13:59 +0600

    project setup

---
+++ src/app/(commonLayout)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+    
+  <div className="flex flex-wrap items-center gap-2 md:flex-row">
+      <Button variant="outline">HOME</Button>
+      
+    </div>
+  );
+}
